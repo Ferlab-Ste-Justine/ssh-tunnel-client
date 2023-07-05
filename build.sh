2f78d0144e863855f8b6def4e5ee528cc570400c
Sébastien Bonami <sbonami@ferlab.bio>
2023-07-05 16:35:44 -0400

    feat: INFRA-344 disable cgo to not depend on shared libraries

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 go generate
 
-env GOOS=linux GOARCH=amd64 go build -o build/linux-amd64/tunnel
-env GOOS=darwin GOARCH=amd64 go build -o build/macos-amd64/tunnel
-env GOOS=windows GOARCH=amd64 go build -o build/windows-amd64/tunnel
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o build/linux-amd64/tunnel
+env GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o build/macos-amd64/tunnel
+env GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o build/windows-amd64/tunnel
